ee96608d91cdba35d8ff3bad4778db3f5f17bb04
chenziwen <chenziwen0413@163.com>
2024-03-17 00:10:01 +0800

    路由守卫

--- chatroom-vue-chat/src/permission.js
+++ chatroom-vue-chat/src/permission.js
@@ -1,8 +1,7 @@
 import router from './router'
-
+import { Message } from "element-ui"
 router.beforeEach((to, from, next) => {
     const userId = window.sessionStorage.getItem('userId')
-
     if (userId) {
         if (to.path === '/login' || to.path === '/') {
             next('/chatroom');
@@ -10,12 +9,14 @@ router.beforeEach((to, from, next) => {
             next();
         }
     } else {
-        if(to.path !== '/login'){
+        //请先登录
+        if (to.path !== '/login') {
+            Message.error("请先登录")
             next('/login');
-        }else{
+        } else {
             next()
         }
-        
+
     }
 
 })
